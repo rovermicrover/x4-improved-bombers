edbd35d0eccfcee9a7cc5708766352bf4c309f48
Andrew Rove <andrew@rove.com>
2022-04-30 23:20:08 -0500

    Misc cleanup

--- publish.cmd
+++ publish.cmd
@@ -5,5 +5,5 @@ xcopy .\content.xml .\release\improvedbombers\
 xcopy .\assets .\release\improvedbombers\assets\ /E
 xcopy .\libraries .\release\improvedbombers\libraries\ /E
 xcopy .\md .\release\improvedbombers\md\ /E
-xcopy .\md .\release\improvedbombers\extensions\ /E
+xcopy .\extensions .\release\improvedbombers\extensions\ /E
 "C:\Program Files (x86)\Steam\steamapps\common\X Tools\WorkshopTool.exe" update -path ".\release\improvedbombers" -buildcat -changenote %1
